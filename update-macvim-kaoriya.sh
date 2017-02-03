7d9d0f8458336bdf6b307f1509a6c006d17a5611
delphinus <delphinus@remora.cx>
2017-02-04 08:39:03 +0900

    Use carton in PATH

--- update-macvim-kaoriya.sh
+++ update-macvim-kaoriya.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 DIR=$(cd $(dirname $0);pwd)
-CARTON=/usr/local/opt/plenv/shims/carton
 cd $DIR
-$CARTON exec -- ./update-macvim-kaoriya.pl $1
+carton exec -- ./update-macvim-kaoriya.pl $1
